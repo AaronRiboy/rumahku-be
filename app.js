622f339a8c0be5defba82bb563b1d431fb327b7a
AaronKajenderan <aaronriboy@gmail.com>
2019-11-07 11:01:36 +0800

    Updated syntax to ES6

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors());
