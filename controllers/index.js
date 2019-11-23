5ed72c7fbd51ed0cae91fad738efbdf2fc99058e
ihlim97 <iehowe97@gmail.com>
2019-11-24 00:09:16 +0800

    Facilities

--- controllers/index.js
+++ controllers/index.js
@@ -10,6 +10,6 @@ router.get('/', (req, res) => res.send('It works!'))
 router.use('/users', UserController)
 router.use('/officers', OfficerController)
 router.use('/residences', ResidencesController)
-router.use('/facility', FacilityController)
+router.use('/facilities', FacilityController)
 
 export default router
